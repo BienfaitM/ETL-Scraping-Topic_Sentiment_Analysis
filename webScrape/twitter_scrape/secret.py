8cc7fb12d7c2f702e0d19384f0047fd5b0186b29
Bienfait <bienfait.murhulamwambali@gmail.com>
2022-01-08 01:32:20 +0300

    clean👨🏾‍💻

--- webScrape/twitter_scrape/secret.py
+++ webScrape/twitter_scrape/secret.py
@@ -3,11 +3,3 @@
 # bearer = '**********'
 # access_token = '***********'
 # access_token_secret = '************'
-
-api_key = 'GiRkXjqKEW87JG8KPLC5BZdIc'
-api_key_secret = 'mIIoG6xj3xkAnakORLscw1pJzDMwsRm0A8E1r2OcREQppzvZqn'
-
-bearer = 'AAAAAAAAAAAAAAAAAAAAALNeWgEAAAAAP7ak76PJxOjfSYywk900U1R60cg%3DbHn7dEf1IEd2LIulykd4dIWrOhGbD4yN3UYeU0Cl9Nq3tgoVlp'
-
-access_token = '1391726391285190656-Kr30zciDGU4tokQeSlJuCujTjX46QG'
-access_token_secret = 'j0uPkyjq5KCr6NxJaiL40gJmks4R2WKousKbHkx55k963'
